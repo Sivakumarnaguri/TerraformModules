ff51687de7194031e4115953c5f063a011561f97
Sivakumar-Naguri <sivakumarnaguri@gmail.com>
2025-06-30 18:40:36 +0530

    Update outputs.tf

--- ec2-module/outputs.tf
+++ ec2-module/outputs.tf
@@ -11,19 +11,3 @@ output "private_ip" {
   description = "Private IP address of the EC2 instance"
   value       = aws_instance.my-ec2.private_ip
 }
-output "instance_state" {
-  description = "State of the EC2 instance"
-  value       = aws_instance.my-ec2.state
-}
-output "instance_type" {
-  description = "Type of the EC2 instance"
-  value       = aws_instance.my-ec2.instance_type
-}
-output "ami" {
-  description = "AMI used for the EC2 instance"
-  value       = aws_instance.my-ec2.ami
-}
-output "key_name" {
-  description = "Key pair name used for the EC2 instance"
-  value       = aws_instance.my-ec2.key_name
-}
